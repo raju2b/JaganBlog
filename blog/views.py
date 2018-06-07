2e1b19a85f830fedb00ff6f934befd0e780715a5
Sowri Raju Bathineni <raju.bathineni@gmail.com>
2018-06-07 22:03:04 +0530

    Demo Blog Application to deploy in Heroku Test App

---
+++ blog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator,PageNotAnInteger, EmptyPage
+from .models import Post
+from .forms import EmailPostForm
+
+def post_list(request):
+    objects_list = Post.published.all()
+    paginator = Paginator(objects_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts})
+
+
+def post_details(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
